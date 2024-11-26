2f662b47e747d05075cfde6a05bc71791a8d655e
Shubhammt <729shubham@gmail.com>
2024-11-26 20:27:39 +0530

    male voice text to speech

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,18 +1,34 @@
 
-from gtts import gTTS
+# from gtts import gTTS
+# import pygame
+# mytext = 'hi, this is testing text to speech for crypto stuff. i am ramu, an average indian crypto enthusiast who thinks only about making money, biiitch'
+# language = 'en'
+# myobj = gTTS(text=mytext, lang=language, slow=False)
+# myobj.save("welcome.mp3")
+# pygame.mixer.init()
+# pygame.mixer.music.load("welcome.mp3")
+# pygame.mixer.music.play()
 
-import pygame
 
-mytext = 'hi, this is testing text to speech for crypto stuff. i am ramu, an average indian crypto enthusiast who thinks only about making money, biiitch'
+import pyttsx3
+from pygame import mixer
 
-language = 'en'
+def speak_and_save(text, filename):
+  engine = pyttsx3.init()
 
-myobj = gTTS(text=mytext, lang=language, slow=False)
+  # Set the voice to a male voice
+  voices = engine.getProperty('voices')
+  engine.setProperty('voice', voices[0].id)  # Adjust the index as needed
 
-myobj.save("welcome.mp3")
+  # Set other properties (optional)
+  engine.setProperty('rate', 150)  # Adjust the speech rate
+  engine.setProperty('volume', 1.0)  # Adjust the volume
 
-pygame.mixer.init()
+  engine.say(text)
+  engine.save_to_file(filename = filename, text = text)
+  engine.runAndWait()
 
-pygame.mixer.music.load("welcome.mp3")
-
-pygame.mixer.music.play()
+# Example usage:
+text = 'hi, this is testing text to speech for crypto stuff. i am ramu, an average indian crypto enthusiast who thinks only about making money, biiitch'
+filename = "welcome.mp3"
+speak_and_save(text, filename)
